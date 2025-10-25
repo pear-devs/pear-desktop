7de9250aee52b01914dd2729ec0f4a1af3126143
Sergio Caguana <undeffineddev419@proton.me>
2025-10-25 18:16:15 -0500

    feat(lyrics): integrate SimpMusic Lyrics provider

--- src/plugins/synced-lyrics/providers/renderer.ts
+++ src/plugins/synced-lyrics/providers/renderer.ts
@@ -3,11 +3,13 @@ import { YTMusic } from './YTMusic';
 import { LRCLib } from './LRCLib';
 import { MusixMatch } from './MusixMatch';
 import { LyricsGenius } from './LyricsGenius';
+import { SimpMusicLyrics } from './SimpMusicLyrics';
 
 export const providers = {
   [ProviderNames.YTMusic]: new YTMusic(),
   [ProviderNames.LRCLib]: new LRCLib(),
   [ProviderNames.MusixMatch]: new MusixMatch(),
   [ProviderNames.LyricsGenius]: new LyricsGenius(),
+  [ProviderNames.SimpMusicLyrics]: new SimpMusicLyrics(),
   // [ProviderNames.Megalobiz]: new Megalobiz(), // Disabled because it is too unstable and slow
 } as const;
