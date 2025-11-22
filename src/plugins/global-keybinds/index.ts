57329a90b6ad90888463b54ad5cd22e83a635e68
Gabriel Pastori <58153955+gabrielpastori1@users.noreply.github.com>
2025-11-22 18:16:58 -0300

    Update src/plugins/global-keybinds/index.ts
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/plugins/global-keybinds/index.ts
+++ src/plugins/global-keybinds/index.ts
@@ -1,5 +1,6 @@
 import { globalShortcut, ipcMain } from 'electron';
 import prompt, { type KeybindOptions } from 'custom-electron-prompt';
+
 import type { BackendContext } from '@/types/contexts';
 
 import { eventRace } from './utils';
