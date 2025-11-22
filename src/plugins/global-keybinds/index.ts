40e3001ef920efbeafd01dd723838d517e56f007
Gabriel Pastori <58153955+gabrielpastori1@users.noreply.github.com>
2025-11-22 18:15:32 -0300

    fix: corrigir a ordem de importação do BackendContext no plugin de keybinds

--- src/plugins/global-keybinds/index.ts
+++ src/plugins/global-keybinds/index.ts
@@ -1,8 +1,8 @@
 import { globalShortcut, ipcMain } from 'electron';
 import prompt, { type KeybindOptions } from 'custom-electron-prompt';
+import type { BackendContext } from '@/types/contexts';
 
 import { eventRace } from './utils';
-import type { BackendContext } from '@/types/contexts';
 import { createPlugin } from '@/utils';
 
 import promptOptions from '@/providers/prompt-options';
