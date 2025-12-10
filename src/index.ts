ytmd-devs/ytmd

misc: replace eslint + prettier with biomejs

src/index.ts
@@ -1,65 +1,54 @@
+import fs from 'node:fs';
 import path from 'node:path';
 import url from 'node:url';
-import fs from 'node:fs';
-
-import {
-  BrowserWindow,
-  app,
-  screen,
-  globalShortcut,
-  session,
-  shell,
-  dialog,
-  ipcMain,
-  protocol,
-  type BrowserWindowConstructorOptions,
-} from 'electron';
+import { languageResources } from 'virtual:i18n';
+import { allPlugins, mainPlugins } from 'virtual:plugins';
+import ErrorHtmlAsset from '@assets/error.html?asset';
 import enhanceWebRequest, {
   type BetterSession,
 } from '@jellybrick/electron-better-web-request';
+import { deepmerge } from 'deepmerge-ts';
+import {
+  app,
+  BrowserWindow,
+  type BrowserWindowConstructorOptions,
+  dialog,
+  globalShortcut,
+  ipcMain,
+  protocol,
+  screen,
+  session,
+  shell,
+} from 'electron';
+import electronDebug from 'electron-debug';
 import is from 'electron-is';
 import unhandled from 'electron-unhandled';
 import { autoUpdater } from 'electron-updater';
-import electronDebug from 'electron-debug';
-import { parse } from 'node-html-parser';
-import { deepmerge } from 'deepmerge-ts';
 import { deepEqual } from 'fast-equals';
-
-import { allPlugins, mainPlugins } from 'virtual:plugins';
-
-import { languageResources } from 'virtual:i18n';
-
+import { parse } from 'node-html-parser';
 import * as config from '@/config';
-
-import { refreshMenu, setApplicationMenu } from '@/menu';
-import { fileExists, injectCSS, injectCSSAsFile } from '@/plugins/utils/main';
-import { isTesting } from '@/utils/testing';
-import { setUpTray } from '@/tray';
-import { setupSongInfo } from '@/providers/song-info';
-import { restart, setupAppControls } from '@/providers/app-controls';
-import {
-  APP_PROTOCOL,
-  handleProtocol,
-  setupProtocolHandler,
-} from '@/providers/protocol-handler';
-
-import youtubeMusicCSS from '@/youtube-music.css?inline';
-
+import { loadI18n, setLanguage, t } from '@/i18n';
 import {
   forceLoadMainPlugin,
   forceUnloadMainPlugin,
   getAllLoadedMainPlugins,
   loadAllMainPlugins,
 } from '@/loader/main';
-
-import { LoggerPrefix } from '@/utils';
-import { loadI18n, setLanguage, t } from '@/i18n';
-
-import ErrorHtmlAsset from '@assets/error.html?asset';
-
+import { refreshMenu, setApplicationMenu } from '@/menu';
 import { defaultAuthProxyConfig } from '@/plugins/auth-proxy-adapter/config';
-
-import type { PluginConfig } from '@/types/plugins';
+import { fileExists, injectCSS, injectCSSAsFile } from '@/plugins/utils/main';
+import { restart, setupAppControls } from '@/providers/app-controls';
+import {
+  APP_PROTOCOL,
+  handleProtocol,
+  setupProtocolHandler,
+} from '@/providers/protocol-handler';
+import { setupSongInfo } from '@/providers/song-info';
+import { setUpTray } from '@/tray';
+import { type PluginConfig } from '@/types/plugins';
+import { LoggerPrefix } from '@/utils';
+import { isTesting } from '@/utils/testing';
+import youtubeMusicCSS from '@/youtube-music.css?inline';
 
 // Catch errors and log them
 unhandled({
@@ -684,7 +673,6 @@ app.whenReady().then(async () => {
           shortcutDetails.target !== appLocation ||
           shortcutDetails.appUserModelId !== appID
         ) {
-          // eslint-disable-next-line @typescript-eslint/only-throw-error
           throw 'needUpdate';
         }
       } catch (error) {
@@ -943,7 +931,9 @@ function removeContentSecurityPolicy(
         !details.responseHeaders['access-control-allow-origin'] &&
         !details.responseHeaders['Access-Control-Allow-Origin']
       ) {
-        details.responseHeaders['access-control-allow-origin'] = ['https://music.youtube.com'];
+        details.responseHeaders['access-control-allow-origin'] = [
+          'https://music.youtube.com',
+        ];
       }
     }
 
@@ -953,6 +943,7 @@ function removeContentSecurityPolicy(
   // When multiple listeners are defined, apply them all
   betterSession.webRequest.setResolver(
     'onHeadersReceived',
+    // biome-ignore lint/suspicious/useAwait: Async function required by setResolver API signature even though no await is used
     async (listeners) => {
       return listeners.reduce(
         async (accumulator, listener) => {

src/plugins/downloader/main/index.ts
@@ -1,45 +1,41 @@
+import { randomBytes } from 'node:crypto';
 import { existsSync, mkdirSync, writeFileSync } from 'node:fs';
 import { join } from 'node:path';
-import { randomBytes } from 'node:crypto';
-
+import { Mutex } from 'async-mutex';
+import { BG, type BgConfig } from 'bgutils-js';
 import { app, type BrowserWindow, dialog, ipcMain } from 'electron';
-import { Innertube, UniversalCache, Utils, YTNodes } from 'youtubei.js';
 import is from 'electron-is';
 import filenamify from 'filenamify';
-import { Mutex } from 'async-mutex';
-import * as NodeID3 from 'node-id3';
-import { BG, type BgConfig } from 'bgutils-js';
 import { lazy } from 'lazy-var';
-
+import { type PlayerErrorMessage } from 'node_modules/youtubei.js/dist/src/parser/nodes';
+import { type VideoInfo } from 'node_modules/youtubei.js/dist/src/parser/youtube';
+import {
+  type Playlist,
+  type TrackInfo,
+} from 'node_modules/youtubei.js/dist/src/parser/ytmusic';
+import { type FormatOptions } from 'node_modules/youtubei.js/dist/src/types';
+import * as NodeID3 from 'node-id3';
+import { Innertube, UniversalCache, Utils, YTNodes } from 'youtubei.js';
+import { t } from '@/i18n';
+import { getNetFetchAsFetch } from '@/plugins/utils/main';
+import {
+  cleanupName,
+  getImage,
+  MediaType,
+  registerCallback,
+  type SongInfo,
+  SongInfoEvent,
+} from '@/providers/song-info';
+import { type BackendContext } from '@/types/contexts';
+import { type GetPlayerResponse } from '@/types/get-player-response';
+import { type DownloaderPluginConfig } from '../index';
+import { DefaultPresetList, type Preset, YoutubeFormatList } from '../types';
 import {
   cropMaxWidth,
   getFolder,
   sendFeedback as sendFeedback_,
   setBadge,
 } from './utils';
-import {
-  registerCallback,
-  cleanupName,
-  getImage,
-  MediaType,
-  type SongInfo,
-  SongInfoEvent,
-} from '@/providers/song-info';
-import { getNetFetchAsFetch } from '@/plugins/utils/main';
-import { t } from '@/i18n';
-
-import { DefaultPresetList, type Preset, YoutubeFormatList } from '../types';
-
-import type { DownloaderPluginConfig } from '../index';
-import type { BackendContext } from '@/types/contexts';
-import type { GetPlayerResponse } from '@/types/get-player-response';
-import type { FormatOptions } from 'node_modules/youtubei.js/dist/src/types';
-import type { VideoInfo } from 'node_modules/youtubei.js/dist/src/parser/youtube';
-import type { PlayerErrorMessage } from 'node_modules/youtubei.js/dist/src/parser/nodes';
-import type {
-  TrackInfo,
-  Playlist,
-} from 'node_modules/youtubei.js/dist/src/parser/ytmusic';
 
 type CustomSongInfo = SongInfo & { trackId?: string };
 
@@ -87,7 +83,6 @@ const sendError = (error: Error, source?: string) => {
   const songNameMessage = source ? `\nin ${source}` : '';
   const cause = error.cause
     ? `\n\n${
-        // eslint-disable-next-line @typescript-eslint/no-base-to-string,@typescript-eslint/restrict-template-expressions
         error.cause instanceof Error ? error.cause.toString() : error.cause
       }`
     : '';
@@ -171,7 +166,6 @@ export const onMainLoad = async ({
       if (interpreterJavascript) {
         // This is a workaround to run the interpreterJavascript code
         // Maybe there is a better way to do this (e.g. https://github.com/Siubaak/sval ?)
-        // eslint-disable-next-line @typescript-eslint/no-implied-eval,@typescript-eslint/no-unsafe-call
         new Function(interpreterJavascript)();
 
         const poTokenResult = await BG.PoToken.generate({

src/providers/song-info-front.ts
@@ -1,16 +1,13 @@
-import { singleton } from './decorators';
-
-import { LikeType, type GetState } from '@/types/datahost-get-state';
-
-import type { YoutubePlayer } from '@/types/youtube-player';
-import type {
-  AlbumDetails,
-  PlayerOverlays,
-  VideoDataChangeValue,
+import { type GetState, LikeType } from '@/types/datahost-get-state';
+import {
+  type AlbumDetails,
+  type PlayerOverlays,
+  type VideoDataChangeValue,
 } from '@/types/player-api-events';
-
-import type { SongInfo } from './song-info';
-import type { VideoDataChanged } from '@/types/video-data-changed';
+import { type VideoDataChanged } from '@/types/video-data-changed';
+import { type YoutubePlayer } from '@/types/youtube-player';
+import { singleton } from './decorators';
+import { type SongInfo } from './song-info';
 
 const DATAUPDATED_FALLBACK_TIMEOUT_MS = 1500;
 
@@ -246,7 +243,6 @@ export const setupSongInfo = (api: YoutubePlayer) => {
   const videoEventDispatcher = async (
     name: string,
     videoData: VideoDataChangeValue,
-    // eslint-disable-next-line @typescript-eslint/require-await
   ) =>
     document.dispatchEvent(
       new CustomEvent<VideoDataChanged>('videodatachange', {

src/renderer.ts
@@ -1,7 +1,15 @@
 import i18next from 'i18next';
-
-import { startingPages } from './providers/extracted-data';
-import { setupSongInfo } from './providers/song-info-front';
+import { t as i18t, loadI18n, setLanguage } from '@/i18n';
+import { type PluginConfig } from '@/types/plugins';
+import { type QueueElement } from '@/types/queue';
+import { type SearchBoxElement } from '@/types/search-box-element';
+import { type YouTubeMusicAppElement } from '@/types/youtube-music-app-element';
+import { type QueueResponse } from '@/types/youtube-music-desktop-internal';
+import { type YoutubePlayer } from '@/types/youtube-player';
+import {
+  defaultTrustedTypePolicy,
+  registerWindowDefaultTrustedTypePolicy,
+} from '@/utils/trusted-types';
 import {
   createContext,
   forceLoadRendererPlugin,
@@ -10,20 +18,8 @@ import {
   getLoadedRendererPlugin,
   loadAllRendererPlugins,
 } from './loader/renderer';
-
-import { loadI18n, setLanguage, t as i18t } from '@/i18n';
-
-import {
-  defaultTrustedTypePolicy,
-  registerWindowDefaultTrustedTypePolicy,
-} from '@/utils/trusted-types';
-
-import type { PluginConfig } from '@/types/plugins';
-import type { YoutubePlayer } from '@/types/youtube-player';
-import type { QueueElement } from '@/types/queue';
-import type { QueueResponse } from '@/types/youtube-music-desktop-internal';
-import type { YouTubeMusicAppElement } from '@/types/youtube-music-app-element';
-import type { SearchBoxElement } from '@/types/search-box-element';
+import { startingPages } from './providers/extracted-data';
+import { setupSongInfo } from './providers/song-info-front';
 
 let api: (Element & YoutubePlayer) | null = null;
 let isPluginLoaded = false;
@@ -88,9 +84,9 @@ async function onApiLoaded() {
   window.ipcRenderer.on('ytmd:seek-by', (_, t: number) => api!.seekBy(t));
   window.ipcRenderer.on('ytmd:shuffle', () => {
     document
-      .querySelector<
-        HTMLElement & { queue: { shuffle: () => void } }
-      >('ytmusic-player-bar')
+      .querySelector<HTMLElement & { queue: { shuffle: () => void } }>(
+        'ytmusic-player-bar',
+      )
       ?.queue.shuffle();
   });
 
@@ -120,17 +116,17 @@ async function onApiLoaded() {
   window.ipcRenderer.on('ytmd:switch-repeat', (_, repeat = 1) => {
     for (let i = 0; i < repeat; i++) {
       document
-        .querySelector<
-          HTMLElement & { onRepeatButtonClick: () => void }
-        >('ytmusic-player-bar')
+        .querySelector<HTMLElement & { onRepeatButtonClick: () => void }>(
+          'ytmusic-player-bar',
+        )
         ?.onRepeatButtonClick();
     }
   });
   window.ipcRenderer.on('ytmd:update-volume', (_, volume: number) => {
     document
-      .querySelector<
-        HTMLElement & { updateVolume: (volume: number) => void }
-      >('ytmusic-player-bar')
+      .querySelector<HTMLElement & { updateVolume: (volume: number) => void }>(
+        'ytmusic-player-bar',
+      )
       ?.updateVolume(volume);
   });
 
@@ -169,9 +165,9 @@ async function onApiLoaded() {
 
   window.ipcRenderer.on('ytmd:toggle-mute', (_) => {
     document
-      .querySelector<
-        HTMLElement & { onVolumeClick: () => void }
-      >('ytmusic-player-bar')
+      .querySelector<HTMLElement & { onVolumeClick: () => void }>(
+        'ytmusic-player-bar',
+      )
       ?.onVolumeClick();
   });
 
@@ -402,10 +398,9 @@ async function onApiLoaded() {
  * YouTube Music still using ES5, so we need to define custom elements using ES5 style
  */
 const defineYTMDTransElements = () => {
+  // biome-ignore lint/suspicious/noEmptyBlockStatements: Empty constructor required for ES5-style custom element definition
   const YTMDTrans = function () {};
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
   YTMDTrans.prototype = Object.create(HTMLElement.prototype);
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
   YTMDTrans.prototype.connectedCallback = function () {
     const that = this as HTMLElement;
     const key = that.getAttribute('key');

pear-devs/pear-desktop

src/index.ts
@@ -1,65 +1,54 @@
+import fs from 'node:fs';
 import path from 'node:path';
 import url from 'node:url';
-import fs from 'node:fs';
-
-import {
-  BrowserWindow,
-  app,
-  screen,
-  globalShortcut,
-  session,
-  shell,
-  dialog,
-  ipcMain,
-  protocol,
-  type BrowserWindowConstructorOptions,
-} from 'electron';
+import { languageResources } from 'virtual:i18n';
+import { allPlugins, mainPlugins } from 'virtual:plugins';
+import ErrorHtmlAsset from '@assets/error.html?asset';
 import enhanceWebRequest, {
   type BetterSession,
 } from '@jellybrick/electron-better-web-request';
+import { deepmerge } from 'deepmerge-ts';
+import {
+  app,
+  BrowserWindow,
+  type BrowserWindowConstructorOptions,
+  dialog,
+  globalShortcut,
+  ipcMain,
+  protocol,
+  screen,
+  session,
+  shell,
+} from 'electron';
+import electronDebug from 'electron-debug';
 import is from 'electron-is';
 import unhandled from 'electron-unhandled';
 import { autoUpdater } from 'electron-updater';
-import electronDebug from 'electron-debug';
-import { parse } from 'node-html-parser';
-import { deepmerge } from 'deepmerge-ts';
 import { deepEqual } from 'fast-equals';
-
-import { allPlugins, mainPlugins } from 'virtual:plugins';
-
-import { languageResources } from 'virtual:i18n';
-
+import { parse } from 'node-html-parser';
 import * as config from '@/config';
-
-import { refreshMenu, setApplicationMenu } from '@/menu';
-import { fileExists, injectCSS, injectCSSAsFile } from '@/plugins/utils/main';
-import { isTesting } from '@/utils/testing';
-import { setUpTray } from '@/tray';
-import { setupSongInfo } from '@/providers/song-info';
-import { restart, setupAppControls } from '@/providers/app-controls';
-import {
-  APP_PROTOCOL,
-  handleProtocol,
-  setupProtocolHandler,
-} from '@/providers/protocol-handler';
-
-import musicPlayerCss from '@/music-player.css?inline';
-
+import { loadI18n, setLanguage, t } from '@/i18n';
 import {
   forceLoadMainPlugin,
   forceUnloadMainPlugin,
   getAllLoadedMainPlugins,
   loadAllMainPlugins,
 } from '@/loader/main';
-
-import { LoggerPrefix } from '@/utils';
-import { loadI18n, setLanguage, t } from '@/i18n';
-
-import ErrorHtmlAsset from '@assets/error.html?asset';
-
+import { refreshMenu, setApplicationMenu } from '@/menu';
+import musicPlayerCss from '@/music-player.css?inline';
 import { defaultAuthProxyConfig } from '@/plugins/auth-proxy-adapter/config';
-
-import type { PluginConfig } from '@/types/plugins';
+import { fileExists, injectCSS, injectCSSAsFile } from '@/plugins/utils/main';
+import { restart, setupAppControls } from '@/providers/app-controls';
+import {
+  APP_PROTOCOL,
+  handleProtocol,
+  setupProtocolHandler,
+} from '@/providers/protocol-handler';
+import { setupSongInfo } from '@/providers/song-info';
+import { setUpTray } from '@/tray';
+import { type PluginConfig } from '@/types/plugins';
+import { LoggerPrefix } from '@/utils';
+import { isTesting } from '@/utils/testing';
 
 // Catch errors and log them
 unhandled({
@@ -687,7 +676,6 @@ app.whenReady().then(async () => {
           shortcutDetails.target !== appLocation ||
           shortcutDetails.appUserModelId !== appID
         ) {
-          // eslint-disable-next-line @typescript-eslint/only-throw-error
           throw 'needUpdate';
         }
       } catch (error) {
@@ -946,7 +934,9 @@ function removeContentSecurityPolicy(
         !details.responseHeaders['access-control-allow-origin'] &&
         !details.responseHeaders['Access-Control-Allow-Origin']
       ) {
-        details.responseHeaders['access-control-allow-origin'] = ['https://music.\u0079\u006f\u0075\u0074\u0075\u0062\u0065.com'];
+        details.responseHeaders['access-control-allow-origin'] = [
+          'https://music.\u0079\u006f\u0075\u0074\u0075\u0062\u0065.com',
+        ];
       }
     }
 
@@ -956,6 +946,7 @@ function removeContentSecurityPolicy(
   // When multiple listeners are defined, apply them all
   betterSession.webRequest.setResolver(
     'onHeadersReceived',
+    // biome-ignore lint/suspicious/useAwait: Async function required by setResolver API signature even though no await is used
     async (listeners) => {
       return listeners.reduce(
         async (accumulator, listener) => {

src/plugins/downloader/main/index.ts
@@ -1,51 +1,51 @@
-import { existsSync, mkdirSync, writeFileSync } from 'node:fs';
-import { join } from 'node:path';
-import { randomBytes } from 'node:crypto';
-
-import { app, type BrowserWindow, dialog, ipcMain } from 'electron';
 import {
   Innertube,
+  Platform,
   UniversalCache,
   Utils,
   YTNodes,
-  Platform,
 } from '\u0079\u006f\u0075\u0074\u0075\u0062\u0065i.js';
+import { randomBytes } from 'node:crypto';
+import { existsSync, mkdirSync, writeFileSync } from 'node:fs';
+import { join } from 'node:path';
+import { Mutex } from 'async-mutex';
+import { BG, type BgConfig } from 'bgutils-js';
+import { app, type BrowserWindow, dialog, ipcMain } from 'electron';
 import is from 'electron-is';
 import filenamify from 'filenamify';
-import { Mutex } from 'async-mutex';
-import * as NodeID3 from 'node-id3';
-import { BG, type BgConfig } from 'bgutils-js';
 import { lazy } from 'lazy-var';
-
+import { type VideoInfo } from 'node_modules/\u0079\u006f\u0075\u0074\u0075\u0062\u0065i.js/dist/src/parser/\u0079\u006f\u0075\u0074\u0075\u0062\u0065';
+import { type PlayerErrorMessage } from 'node_modules/\u0079\u006f\u0075\u0074\u0075\u0062\u0065i.js/dist/src/parser/nodes';
+import {
+  type Playlist,
+  type TrackInfo,
+} from 'node_modules/\u0079\u006f\u0075\u0074\u0075\u0062\u0065i.js/dist/src/parser/ytmusic';
+import {
+  type BuildScriptResult,
+  type FormatOptions,
+  type VMPrimative,
+} from 'node_modules/\u0079\u006f\u0075\u0074\u0075\u0062\u0065i.js/dist/src/types';
+import * as NodeID3 from 'node-id3';
+import { t } from '@/i18n';
+import { getNetFetchAsFetch } from '@/plugins/utils/main';
+import {
+  cleanupName,
+  getImage,
+  MediaType,
+  registerCallback,
+  type SongInfo,
+  SongInfoEvent,
+} from '@/providers/song-info';
+import { type BackendContext } from '@/types/contexts';
+import { type GetPlayerResponse } from '@/types/get-player-response';
+import { type DownloaderPluginConfig } from '../index';
+import { DefaultPresetList, type Preset, VideoFormatList } from '../types';
 import {
   cropMaxWidth,
   getFolder,
   sendFeedback as sendFeedback_,
   setBadge,
 } from './utils';
-import {
-  registerCallback,
-  cleanupName,
-  getImage,
-  MediaType,
-  type SongInfo,
-  SongInfoEvent,
-} from '@/providers/song-info';
-import { getNetFetchAsFetch } from '@/plugins/utils/main';
-import { t } from '@/i18n';
-
-import { DefaultPresetList, type Preset, VideoFormatList } from '../types';
-
-import type { DownloaderPluginConfig } from '../index';
-import type { BackendContext } from '@/types/contexts';
-import type { GetPlayerResponse } from '@/types/get-player-response';
-import type { FormatOptions } from 'node_modules/\u0079\u006f\u0075\u0074\u0075\u0062\u0065i.js/dist/src/types';
-import type { VideoInfo } from 'node_modules/\u0079\u006f\u0075\u0074\u0075\u0062\u0065i.js/dist/src/parser/\u0079\u006f\u0075\u0074\u0075\u0062\u0065';
-import type { PlayerErrorMessage } from 'node_modules/\u0079\u006f\u0075\u0074\u0075\u0062\u0065i.js/dist/src/parser/nodes';
-import type {
-  TrackInfo,
-  Playlist,
-} from 'node_modules/\u0079\u006f\u0075\u0074\u0075\u0062\u0065i.js/dist/src/parser/ytmusic';
 
 type CustomSongInfo = SongInfo & { trackId?: string };
 
@@ -58,21 +58,25 @@ const ffmpeg = lazy(async () =>
 );
 const ffmpegMutex = new Mutex();
 
-Platform.shim.eval = async (data: Types.BuildScriptResult, env: Record<string, Types.VMPrimative>) => {
+// biome-ignore lint/suspicious/useAwait: I'll leave this async as I don't want to break anything
+Platform.shim.eval = async (
+  data: BuildScriptResult,
+  env: Record<string, VMPrimative>,
+) => {
   const properties = [];
 
-  if(env.n) {
-    properties.push(`n: exportedVars.nFunction("${env.n}")`)
+  if (env.n) {
+    properties.push(`n: exportedVars.nFunction("${env.n}")`);
   }
 
   if (env.sig) {
-    properties.push(`sig: exportedVars.sigFunction("${env.sig}")`)
+    properties.push(`sig: exportedVars.sigFunction("${env.sig}")`);
   }
 
   const code = `${data.output}\nreturn { ${properties.join(', ')} }`;
 
   return new Function(code)();
-}
+};
 
 let yt: Innertube;
 let win: BrowserWindow;
@@ -109,7 +113,6 @@ const sendError = (error: Error, source?: string) => {
   const songNameMessage = source ? `\nin ${source}` : '';
   const cause = error.cause
     ? `\n\n${
-        // eslint-disable-next-line @typescript-eslint/no-base-to-string,@typescript-eslint/restrict-template-expressions
         error.cause instanceof Error ? error.cause.toString() : error.cause
       }`
     : '';
@@ -192,7 +195,6 @@ export const onMainLoad = async ({
       if (interpreterJavascript) {
         // This is a workaround to run the interpreterJavascript code
         // Maybe there is a better way to do this (e.g. https://github.com/Siubaak/sval ?)
-        // eslint-disable-next-line @typescript-eslint/no-implied-eval,@typescript-eslint/no-unsafe-call
         new Function(interpreterJavascript)();
 
         const poTokenResult = await BG.PoToken.generate({
@@ -421,8 +423,7 @@ async function downloadSongUnsafe(
   let targetFileExtension: string;
   if (!presetSetting?.extension) {
     targetFileExtension =
-      VideoFormatList.find((it) => it.itag === format.itag)?.container ??
-      'mp3';
+      VideoFormatList.find((it) => it.itag === format.itag)?.container ?? 'mp3';
   } else {
     targetFileExtension = presetSetting?.extension ?? 'mp3';
   }

src/providers/song-info-front.ts
@@ -1,16 +1,13 @@
-import { singleton } from './decorators';
-
-import { LikeType, type GetState } from '@/types/datahost-get-state';
-
-import type { MusicPlayer } from '@/types/music-player';
-import type {
-  AlbumDetails,
-  PlayerOverlays,
-  VideoDataChangeValue,
+import { type GetState, LikeType } from '@/types/datahost-get-state';
+import { type MusicPlayer } from '@/types/music-player';
+import {
+  type AlbumDetails,
+  type PlayerOverlays,
+  type VideoDataChangeValue,
 } from '@/types/player-api-events';
-
-import type { SongInfo } from './song-info';
-import type { VideoDataChanged } from '@/types/video-data-changed';
+import { type VideoDataChanged } from '@/types/video-data-changed';
+import { singleton } from './decorators';
+import { type SongInfo } from './song-info';
 
 const DATAUPDATED_FALLBACK_TIMEOUT_MS = 1500;
 
@@ -246,7 +243,6 @@ export const setupSongInfo = (api: MusicPlayer) => {
   const videoEventDispatcher = async (
     name: string,
     videoData: VideoDataChangeValue,
-    // eslint-disable-next-line @typescript-eslint/require-await
   ) =>
     document.dispatchEvent(
       new CustomEvent<VideoDataChanged>('videodatachange', {

src/renderer.ts
@@ -4,8 +4,17 @@ import { setTheme } from 'mdui/functions/setTheme.js';
 import 'mdui/mdui.css';
 import 'mdui';
 
-import { startingPages } from './providers/extracted-data';
-import { setupSongInfo } from './providers/song-info-front';
+import { t as i18t, loadI18n, setLanguage } from '@/i18n';
+import { type MusicPlayer } from '@/types/music-player';
+import { type MusicPlayerAppElement } from '@/types/music-player-app-element';
+import { type QueueResponse } from '@/types/music-player-desktop-internal';
+import { type PluginConfig } from '@/types/plugins';
+import { type QueueElement } from '@/types/queue';
+import { type SearchBoxElement } from '@/types/search-box-element';
+import {
+  defaultTrustedTypePolicy,
+  registerWindowDefaultTrustedTypePolicy,
+} from '@/utils/trusted-types';
 import {
   createContext,
   forceLoadRendererPlugin,
@@ -14,20 +23,8 @@ import {
   getLoadedRendererPlugin,
   loadAllRendererPlugins,
 } from './loader/renderer';
-
-import { loadI18n, setLanguage, t as i18t } from '@/i18n';
-
-import {
-  defaultTrustedTypePolicy,
-  registerWindowDefaultTrustedTypePolicy,
-} from '@/utils/trusted-types';
-
-import type { PluginConfig } from '@/types/plugins';
-import type { MusicPlayer } from '@/types/music-player';
-import type { QueueElement } from '@/types/queue';
-import type { QueueResponse } from '@/types/music-player-desktop-internal';
-import type { MusicPlayerAppElement } from '@/types/music-player-app-element';
-import type { SearchBoxElement } from '@/types/search-box-element';
+import { startingPages } from './providers/extracted-data';
+import { setupSongInfo } from './providers/song-info-front';
 
 setTheme('dark');
 
@@ -94,9 +91,9 @@ async function onApiLoaded() {
   window.ipcRenderer.on('peard:seek-by', (_, t: number) => api!.seekBy(t));
   window.ipcRenderer.on('peard:shuffle', () => {
     document
-      .querySelector<
-        HTMLElement & { queue: { shuffle: () => void } }
-      >('ytmusic-player-bar')
+      .querySelector<HTMLElement & { queue: { shuffle: () => void } }>(
+        'ytmusic-player-bar',
+      )
       ?.queue.shuffle();
   });
 
@@ -126,17 +123,17 @@ async function onApiLoaded() {
   window.ipcRenderer.on('peard:switch-repeat', (_, repeat = 1) => {
     for (let i = 0; i < repeat; i++) {
       document
-        .querySelector<
-          HTMLElement & { onRepeatButtonClick: () => void }
-        >('ytmusic-player-bar')
+        .querySelector<HTMLElement & { onRepeatButtonClick: () => void }>(
+          'ytmusic-player-bar',
+        )
         ?.onRepeatButtonClick();
     }
   });
   window.ipcRenderer.on('peard:update-volume', (_, volume: number) => {
     document
-      .querySelector<
-        HTMLElement & { updateVolume: (volume: number) => void }
-      >('ytmusic-player-bar')
+      .querySelector<HTMLElement & { updateVolume: (volume: number) => void }>(
+        'ytmusic-player-bar',
+      )
       ?.updateVolume(volume);
   });
 
@@ -175,9 +172,9 @@ async function onApiLoaded() {
 
   window.ipcRenderer.on('peard:toggle-mute', (_) => {
     document
-      .querySelector<
-        HTMLElement & { onVolumeClick: () => void }
-      >('ytmusic-player-bar')
+      .querySelector<HTMLElement & { onVolumeClick: () => void }>(
+        'ytmusic-player-bar',
+      )
       ?.onVolumeClick();
   });
 