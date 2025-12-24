af261bc36cea5bb9520b666cd88397feebb37db6
its-iris <244833241+its-iris@users.noreply.github.com>
2025-12-24 22:57:08 +0100

    feat(transparent-player): Added Linux and macOS support

--- src/plugins/transparent-player/types.ts
+++ src/plugins/transparent-player/types.ts
@@ -1,8 +1,17 @@
 export enum MaterialType {
+  NONE = 'none',
+
+  // Windows materials
   MICA = 'mica',
   ACRYLIC = 'acrylic',
   TABBED = 'tabbed',
-  NONE = 'none',
+
+  // macOS materials
+  WINDOW = 'window',
+  FULLSCREEN_UI = 'fullscreen-ui',
+  CONTENT = 'content',
+  UNDER_WINDOW = 'under-window',
+  UNDER_PAGE = 'under-page',
 }
 
 export type TransparentPlayerConfig = {
